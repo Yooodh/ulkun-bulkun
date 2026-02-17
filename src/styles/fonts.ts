98c802755c41cded5fce5d907f1716d9e2682564
Yo_o. <eogus2604@hanmail.net>
2026-02-17 21:55:20 +0900

    feat: 로컬 폰트 추가 및 컬러 변수 리팩토링 (#7)
    * feat: 로컬 폰트 등록 및 폰트 스타일 믹스인 추가
    
    * refactor: 컬러 변수 구조 개선 및 색상 팔레트 재정의

---
+++ src/styles/fonts.ts
@@ -0,0 +1,22 @@
+import localFont from 'next/font/local';
+
+export const myTitleFont = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Title_Light.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Title_Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Title_Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-title',
+});
