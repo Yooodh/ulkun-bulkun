a1596899a061318aea46aa2587fcbca19232d5aa
Yo_o. <eogus2604@hanmail.net>
2026-02-18 00:10:42 +0900

    feat: 공용 Button 컴포넌트 구현 (#8)
    * feat: Button 컴포넌트 구현
    
    * style: Button 컴포넌트 스타일 추가

---
+++ src/components/shared/Button/Button.tsx
@@ -0,0 +1,25 @@
+import styles from './Button.module.scss';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'gray' | 'red' | 'ligray';
+  size?: 'sm' | 'md' | 'lg';
+  shape?: 'square' | 'round';
+};
+
+export default function Button({
+  children,
+  variant = 'gray',
+  size = 'md',
+  shape = 'square',
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={`${styles.button} ${styles[variant]} ${styles[size]} ${styles[shape]} ${className || ''}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
