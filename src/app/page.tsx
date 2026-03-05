b2a319fd39d1b77ce0dba09e6d6b1c124301623d
Yo_o. <eogus2604@hanmail.net>
2026-03-06 00:04:29 +0900

    feat: 홈 화면 및 루트 레이아웃 리팩토링 및 인증 UI 통합 (#22)
    * feat: 로그인 상태에 따라 대시보드 및 게스트 화면 분기 로직 구현
    
    * style: 대시보드 및 게스트 화면 스타일 추가
    
    * feat: AuthForm 및 폰트 스타일 추가
    
    * style: 루트 레이아웃 스타일 추가
    
    * style: 전역 스타일 위치 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import styles from './page.module.scss';
+
+import RecordForm from '@/components/RecordForm/RecordForm';
+import RecordList from '@/components/RecordList/RecordList';
+import RecordChart from '@/components/RecordChart/RecordChart';
+import ProfileCard from '@/components/Profile';
+import Empty from '@/components/shared/Empty/Empty';
+
+import { useAuth } from '@/hooks/useAuth';
+
 export default function Home() {
+  const { user, loading } = useAuth();
+
+  if (loading) return null;
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className={styles.pageContainer}>
+      <div className={`${styles.dashboardGrid} ${!user ? styles.isGuest : ''}`}>
+        <RecordForm />
+
+        {user ? (
+          <>
+            <ProfileCard />
+            <RecordChart />
+            <RecordList />
+          </>
+        ) : (
+          <div className={styles.guestMessage}>
+            <Empty
+              message='로그인 후 기록을 시작해 볼까요?'
+              subMessage='나만의 운동 통계와 변화 그래프를 확인해 보세요.'
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
