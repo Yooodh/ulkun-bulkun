21ef1ff394640f15c095f1edf3c4a41ff667216c
Yooodh <eogus2604@hanmail.net>
2026-03-21 00:31:31 +0900

    feat: 외부 이미지 도메인 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
