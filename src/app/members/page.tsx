b189871cc2f9fe85a9a4918101f983b7ac7fa2c9
Yooodh <eogus2604@hanmail.net>
2026-04-13 18:41:54 +0900

    feat: 리팩토링 및 정렬 로직 추가

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,19 +1,34 @@
 'use client';
 
-import Link from 'next/link';
-import Image from 'next/image';
+import { useMemo } from 'react';
 
 import styles from './Members.module.scss';
+
+import UserCard from './components/UserCard/UserCard';
 
 import Loading from '@/components/shared/Loading/Loading';
 import NavBar from '@/components/shared/NavBar/NavBar';
 
 import { useUsers } from '@/hooks/useUsers';
 
-import { formatDate } from '@/utils/dateUtils';
-
 export default function MembersPage() {
   const { users, loading } = useUsers();
+
+  const sortedUsers = useMemo(() => {
+    return [...users].sort((a, b) => {
+      if (a.is_public !== b.is_public) return a.is_public ? -1 : 1;
+
+      const totalA =
+        (a.max_squat || 0) + (a.max_bench || 0) + (a.max_deadlift || 0);
+      const totalB =
+        (b.max_squat || 0) + (b.max_bench || 0) + (b.max_deadlift || 0);
+
+      const finalA = totalA || a.max_total || 0;
+      const finalB = totalB || b.max_total || 0;
+
+      return finalB - finalA;
+    });
+  }, [users]);
 
   if (loading)
     return <Loading fullHeight={true} message='울끈불끈이들 입장 중!' />;
@@ -21,49 +36,11 @@ export default function MembersPage() {
   return (
     <div className={styles.membersContainer}>
       <NavBar href='/' label='대시보드로 돌아가기' />
-
       <h1 className={styles.title}>🔥 울끈불끈이들 🔥</h1>
 
       <div className={styles.userGrid}>
-        {users.map((user) => (
-          <Link
-            href={`/members/${user.id}`}
-            key={user.id}
-            className={styles.userCard}
-          >
-            <div className={styles.avatarWrapper}>
-              {user.avatar_url ? (
-                <Image
-                  src={user.avatar_url}
-                  alt={user.nickname}
-                  width={50}
-                  height={50}
-                  className={styles.avatar}
-                />
-              ) : (
-                <div className={styles.defaultAvatar}>💪</div>
-              )}
-            </div>
-            <div className={styles.userInfo}>
-              <p className={styles.statusMessage}>{user.status_message}</p>
-
-              <h3 className={styles.userName}>{user.nickname}</h3>
-
-              <p className={styles.userStats}>
-                PR{' '}
-                <span>
-                  {user.max_total && user.max_total > 0
-                    ? `${user.max_total}kg`
-                    : '기록 없음'}
-                </span>
-              </p>
-
-              <p className={styles.date}>
-                마지막 기록{''}
-                <span> {formatDate(user.last_activity)}</span>
-              </p>
-            </div>
-          </Link>
+        {sortedUsers.map((user) => (
+          <UserCard key={user.id} user={user} />
         ))}
       </div>
     </div>
