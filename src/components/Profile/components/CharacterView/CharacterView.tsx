3a715eb22aa4c4bfe29f25b7bfa73cfd0b24595a
Yooodh <eogus2604@hanmail.net>
2026-05-05 03:13:43 +0900

    feat: 캐릭터 로딩 상태에 따라 Canvas 렌더링

--- src/components/Profile/components/CharacterView/CharacterView.tsx
+++ src/components/Profile/components/CharacterView/CharacterView.tsx
@@ -2,26 +2,27 @@
 
 import styles from './CharacterView.module.scss';
 
-import Empty from '@/components/shared/Empty/Empty';
+import Canvas from './Canvas';
+
 import Loading from '@/components/shared/Loading/Loading';
 
 type CharacterViewProps = {
   isLoading?: boolean;
+  totalPR?: number;
 };
 
-export default function CharacterView({ isLoading }: CharacterViewProps) {
+export default function CharacterView({
+  isLoading,
+  totalPR = 0,
+}: CharacterViewProps) {
   return (
     <div className={styles.characterContainer}>
       <div>
         {isLoading ? (
           <Loading message='캐릭터를 불러오고 있어요!' />
         ) : (
-          <Empty
-            message='조금만 기다려주세요.'
-            subMessage='곧 나만의 캐릭터를 만나보실 수 있어요.'
-          />
+          <Canvas totalPR={totalPR} />
         )}
-        {/* <Canvas /> */}
       </div>
     </div>
   );
