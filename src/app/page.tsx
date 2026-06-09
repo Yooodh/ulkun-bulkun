9f8c632f10f1203e06aa9595c7e0acdabddc9eac
Yooodh <eogus2604@hanmail.net>
2026-06-10 00:00:55 +0900

    refactor: 로그인 상태에 따라 대시보드 레이아웃 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,24 +21,24 @@ export default function Home() {
     <div className={styles.pageContainer}>
       <NavBar href='/members' label='🔥 울끈불끈이들 기록 보러가기 🔥' />
 
-      <div className={`${styles.dashboardGrid} ${!user ? styles.isGuest : ''}`}>
-        <RecordForm />
-
-        {user ? (
-          <>
-            <ProfileCard />
-            <Charts />
-            <RecordList />
-          </>
-        ) : (
+      {user ? (
+        <div className={styles.dashboardGrid}>
+          <RecordForm />
+          <ProfileCard />
+          <Charts />
+          <RecordList />
+        </div>
+      ) : (
+        <div className={styles.guestGrid}>
+          <RecordForm />
           <div className={styles.guestMessage}>
             <Empty
               message='로그인 후 기록을 시작해 볼까요?'
               subMessage='나만의 운동 통계와 변화 그래프를 확인해 보세요.'
             />
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
