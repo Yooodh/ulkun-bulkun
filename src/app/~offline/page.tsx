54097649cb5902d3c6db25c21c22ff24e3f5fb80
Yo_o. <eogus2604@hanmail.net>
2026-07-11 01:25:53 +0900

    fix: PWA 프리캐싱 오류 해결을 위한 오프라인 페이지 추가 (#142)
    * feat: PWA 프리캐싱 오류 해결을 위한 오프라인 페이지 추가
    
    * style: 오프라인 페이지 레이아웃 스타일 추가
    
    * feat: PWA 프리캐싱 오류 해결을 위한 오프라인 페이지 추가

---
+++ src/app/~offline/page.tsx
@@ -0,0 +1,10 @@
+import styles from './Offline.module.scss';
+
+export default function OfflinePage() {
+  return (
+    <div className={styles.container}>
+      <h1>오프라인 상태예요</h1>
+      <p>인터넷 연결을 확인한 후 다시 시도해주세요.</p>
+    </div>
+  );
+}
